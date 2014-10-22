44d8bbc15aad4c657d1a562065bbda63a2aaaedc
jiangyaz <jiangyaz@usc.edu>
2014-10-22 12:48:59 -0700

    10/22 noon

--- other/patternMatch.cpp
+++ other/patternMatch.cpp
@@ -1,71 +1,75 @@
-
-
 void tokenize(string str, vector<string> &tokens) {
-	int index = 0;
-	while (index < str.length()) {
-		string token;
-		// Skip the initial empty spaces
-		while (str[index] == ' ' && index < str.length()) {
-			index++;
-		}
-		// Loop through the non-space characters until hit space
-		while (str[index] != ' ' && index < str.length()) {
-			token += str[index++];
-		}
-		if (token.length() > 0) {
-			tokens.push_back(token);
-		}
-	}
+    int index = 0;
+    while (index < str.length()) {
+        string token;
+        // Skip the initial empty spaces
+        while (str[index] == ' ' && index < str.length()) {
+            index++;
+        }
+        // Loop through the non-space characters until hit space
+        while (str[index] != ' ' && index < str.length()) {
+            token += str[index++];
+        }
+        if (token.length() > 0) {
+            tokens.push_back(token);
+        }
+    }
 }
-
 
 // With space
 bool patternMatch1(string p, string s) {
-	unordered_map<string, string> mp;
-	vector<string> pToken, sToken;
-	tokenize(p, pToken);
-	tokenize(s, sToken);
+    unordered_map<string, string> mp;
+    vector<string> pToken, sToken;
+    // Tokenize the strings
+    tokenize(p, pToken);
+    tokenize(s, sToken);
 
-	if (pToken.size() != s.Token.size()) return false;
-	for (int i = 0; i < pToken.size(); i++) {
-		// If the token is not in the map
-		if (mp.find(pToken[i]) == mp.end()) {
-			mp[pToken[i]] = sToken[i];
-		} else {
-			if (mp[pToken] != sToken[i]) 
-				return false;
-		}
-	}
-	return true;
+    // First check if size is the same
+    if (pToken.size() != sToken.size()) 
+        return false;
+
+    for (int i = 0; i < pToken.size(); i++) {
+        // If the token is not in the map
+        if (mp.find(pToken[i]) == mp.end()) {
+            mp[pToken[i]] = sToken[i];
+        } else {
+            if (mp[pToken[i]] != sToken[i]) 
+                return false;
+        }
+    }
+    return true;
 } 
 
+
+bool matchPattern(string p, int startP, string s, int startS, unordered_map<char, string> &mp) {
+    if (startP == p.length()) {
+        return startS == s.length();
+    }
+
+    // First character of p is already in the map
+    if (mp.find(p[startP]) != mp.end()) {
+        int len = mp[p[startP]].length();
+        if (mp[p[startP]] != s.substr(startS, len)) 
+            return false;
+        else
+            return matchPattern(p, startP+1, s, startS + len, mp); 
+    }
+
+    // Loop through all posibilities of string s
+    for (int i = startS; i < s.length(); i++) {
+        string word = s.substr(startS, i-startS+1);
+        mp[p[startP]] = word;
+        if(matchPattern(p, startP+1, s, i+1, mp)) 
+            return true;
+        mp.erase(p[startP]);
+    }
+    return false;
+}
 
 // Without space
 bool patternMatch2(string p, string s) {
-	unordered_map<char, string> mp;
-	return matchPattern(p, 0, s, 0, mp);
+    unordered_map<char, string> mp;
+    return matchPattern(p, 0, s, 0, mp);
 } 
-
-bool matchPattern(string p, int startP, string s, int startS, unordered_map<char, string> &mp) {
-	if (startP == p.length() && startS == s.length()) return true;
-
-	for (int i = startS; i < s.length(); i++) {
-		string word = s.subStr(startS, i-startS+1);
-		if (mp.find(p[startP]) == mp.end()) {    // If not found, add
-			mp[p[startP]] = word;
-			return matchPattern(p, startP+1, s, i+1, mp);
-			mp.erase(p[startP]);
-		} else {
-			if (mp[p[startP]] == word) {
-				return matchPattern(p, startP+1, s, i+1, mp);
-			}
-		}
-	}
-	return false;
-}
-
-
-
-
 
 
